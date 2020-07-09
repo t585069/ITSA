6d5e2022f559429ee7be773c3b26a1cd9ada953b
t585069 <t585069@gmail.com>
2020-07-09 11:23:26 +0800

    Update ITSA73 Program.2.cpp

--- ITSA73 Program.2.cpp
+++ ITSA73 Program.2.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 using namespace std;
 
+// è¤‡æ•¸çš„åŠ æ¸›ä¹˜
+
 int main(){
 	double M=0.0, K=0.0, M1=0, K1=0, M2=0, K2=0;
 	
@@ -9,17 +11,17 @@ int main(){
 	cin>>M1>>K1;
 	cin>>M2>>K2;	
 
-// ¥[		
+// åŠ 		
 	M = M1 + M2;
 	K = K1 + K2;
 	cout<<"("<< fixed  <<  setprecision(2) << M <<")+("<< fixed  <<  setprecision(2) << K <<")i"<<endl; 
 	
-// ´î
+// æ¸›
 	M = M1 - M2;
 	K = K1 - K2;
 	cout<<"("<< fixed  <<  setprecision(2) << M <<")+("<< fixed  <<  setprecision(2) << K <<")i"<<endl; 
 	
-// ­¼ 
+// ä¹˜ 
 	M = (M1 * M2) + (-1)*(K1 * K2);
 	K = (M1 * K2) + (M2 * K1);
 	cout<<"("<< fixed  <<  setprecision(2) << M <<")+("<< fixed  <<  setprecision(2) << K <<")i"<<endl; 
